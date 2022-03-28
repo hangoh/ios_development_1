d1f430b91da3004d12b05cf88d77afb69ee189e5
hangoh <gohyuhan12345@gmail.com>
2022-03-28 22:19:04 +0900

    first commit

--- DiceRoll/ViewController.swift
+++ DiceRoll/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var numberup = 0
+    var numberdown = 5
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBOutlet weak var totalSum: UILabel!
+    
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        let diceImageArray = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+        let dice1 = Int.random(in: 0...5)
+        let dice2 = Int.random(in: 0...5)
+        
+        diceImageView1.image = UIImage(named:diceImageArray[dice1])
+        diceImageView2.image = UIImage(named:diceImageArray[dice2])
+        
+        totalSum.text = String(2+dice1+dice2)
+        
     }
-
-
+    
 }
 
